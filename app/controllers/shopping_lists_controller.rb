92785895bd05b3f4f8d8c688c44b663047b49afc
Avelina Maier <maieravelina@gmail.com>
2022-09-06 18:01:09 -0500

    worked on shopping list partial

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,3 +1,24 @@
 class ShoppingListsController < ApplicationController
 
+  def index
+    @shopping_lists = ShoppingList.all
+  end
+
+  def create
+    @shopping_list = ShoppingList.new(shopping_list_params)
+
+    respond_to do |format|
+      if @shopping_list.save
+        format.html { redirect_to space_shopping_list_path, notice: "Item was successfully added" }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def shopping_list_params
+    params.require(:shopping_list).permit(:name)
+  end
 end
